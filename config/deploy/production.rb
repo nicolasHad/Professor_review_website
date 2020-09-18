5fea067115f3a2b2255ba0bec58e3d619a1559c2
Nicolas Hadjisavvas <nicolashadjisavvas@gmail.com>
2020-09-18 09:20:49 +0300

    Update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '167.71.142.0', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
